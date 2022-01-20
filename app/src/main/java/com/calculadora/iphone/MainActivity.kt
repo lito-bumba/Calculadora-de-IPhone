85c4ab51ed73c48037d1c75b6f9e82669b220c3b
lito-bumba <litocdbumba@gmail.com>
2022-01-20 17:10:24 -0300

    Desenhando a Tela

--- app/src/main/java/com/calculadora/iphone/MainActivity.kt
+++ app/src/main/java/com/calculadora/iphone/MainActivity.kt
@@ -2,10 +2,14 @@ package com.calculadora.iphone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.calculadora.iphone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
